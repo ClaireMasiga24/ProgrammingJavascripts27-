234a25f9d43b0fd3b9962a868b4d24b5b24b44eb
Claire Masiga <ClaireMasiga22@gmail.com>
2024-07-03 09:08:03 +0300

    Added variables to and datatypes

--- Hello.js
+++ Hello.js
@@ -1,1 +1,22 @@
+// Prints Hello World to console
 console.log("Hello World!")
+// This is my first comment in JavaScript
+/* My name is Claire
+ I'm trying to
+  code Javascript*/
+console.log("Hello,World!")
+// Printing on different lines in the console
+console.log("Hello\n World!")
+
+console.log("Hello", "World")
+//Addition in Javascript
+console.log(1+1)
+//Subtraction in Javascript
+console.log(4*4)
+// Division with remainder
+console.log(4%3)
+//division 
+console.log(5/5)
+// power operands
+console.log(3**2)
+
---
+++ datatypesAndvariables.js
@@ -0,0 +1,29 @@
+// numbers
+//strings/letters
+//boolean
+//undefined 
+//null
+console.log(typeof "Claire") // string and it has to be quoted
+console.log(typeof 8)//integer
+console.log(typeof 8.5)//a float point number
+console.log(typeof null)//undefined
+console.log(typeof false)
+let emptyValue=null
+let firstName="Claire"
+console.log(firstName ) 
+
+// Variables
+let x=2
+console.log(x)
+//Dont write a variable name with a number , use camel case naming, no space,and  dont start with a character
+ let capitalCity= "Kampala"
+ let isMarried= true
+
+ const PI= 3.14
+ 
+
+
+
+
+
+
